0fe1d261e48664eea8396d03c8c04883c8135f10
jashshah1997 <jashshah37@yahoo.com>
2022-02-26 13:15:04 +0100

    Add goal and Ball

--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -6,6 +6,18 @@ public class GameManager : Singleton<GameManager>
 {
     public bool cursorActive = true;
 
+    [SerializeField]
+    public float ShrinkSpeed = 0.2f;
+
+    private GameObject m_left_wall;
+    private GameObject m_right_wall;
+
+    private void Start()
+    {
+        m_left_wall = GameObject.Find("LeftWall");
+        m_right_wall = GameObject.Find("RightWall");
+    }
+
     void EnableCursor(bool enable)
     {
         if (enable)
@@ -30,5 +42,11 @@ public class GameManager : Singleton<GameManager>
     private void OnDisable()
     {
         AppEvents.MouseCursorEnabled -= EnableCursor;
+    }
+
+    private void FixedUpdate()
+    {
+        m_left_wall.gameObject.transform.position -= new Vector3(ShrinkSpeed * Time.deltaTime, 0f, 0f);
+        m_right_wall.gameObject.transform.position += new Vector3(ShrinkSpeed * Time.deltaTime, 0f, 0f);
     }
 }
